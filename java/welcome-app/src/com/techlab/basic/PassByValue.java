56dd29ee6bcef0ce34bf880f54a6283e01bf8375
sonamsingh15 <sonamsingh15>
2019-10-18 12:39:39 +0530

    java/

--- java/welcome-app/src/com/techlab/basic/PassByValue.java
+++ java/welcome-app/src/com/techlab/basic/PassByValue.java
@@ -3,15 +3,29 @@ package com.techlab.basic;
 public class PassByValue {
 	public static void main(String args[]) {
 	
-	        int x = 5; 
-	        change(x); 
-	        System.out.println("value of x is:" +x); 
+	        int marks= 100; 
+	        mutateMarksToZero(marks); 
+	        System.out.println("Marks is:" +marks); 
+	        int[] marks1= {10,20,30,40};
+	        mutateMarksToZero(marks1); 
+	        for (int m : marks1) {
+	        	System.out.println("marks of array are" +m);
+				
+			}
+	        
 	    } 
-	    public static void change(int x) 
+	    public static void mutateMarksToZero(int marks) 
 	    { 
-	        x = 10; 
+	        marks=0;
 	    } 
-		
+	    public static void mutateMarksToZero(int []marks1) 
+	    { 
+	       for (int i=0;i<4;i++)
+	       {
+	    	   marks1[i]=0;
+	       }
+	    } 
+	    
 	}
 
 
--- java/welcome-app/src/com/techlab/basic/QueryString1.java
+++ java/welcome-app/src/com/techlab/basic/QueryString1.java
@@ -5,12 +5,24 @@ public class QueryString1 {
 		String str = "https://www.swabhavtechlab.com@developer=sonam";
 		
 		//String[] arrofstr=str.split("s",7);
-		String[] arrofstr1=str.split("@");
-		//String[] arrofstr1=str.split("https://");
+	    //String[] arrofstr1=str.split("="); // correct op
+		String[] arrofstr1=str.split(".com@developer=");
+
+		//String[] arrofstr1=str.split("[, @ // :]+");
+		
 		for(String a:arrofstr1)
 		{
 		System.out.println(a);
 		}
+		
+	   /* String s1 = "";
+	    String[] s2 = str.split(".com@developer=");
+
+	    for ( int i = 0; i < s2.length; i++)
+	    {
+	        s1 = s1 + s2[i];
+	    }
+	    System.out.println(s1);*/
 		
 		
 	}
