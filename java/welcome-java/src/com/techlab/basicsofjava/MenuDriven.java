bf95f0f896102a86adc1684a7948333fde7e4124
sonamsingh15 <sonamsingh15>
2019-10-22 16:42:18 +0530

    java/

--- java/welcome-java/src/com/techlab/basicsofjava/MenuDriven.java
+++ java/welcome-java/src/com/techlab/basicsofjava/MenuDriven.java
@@ -1,19 +1,47 @@
 package com.techlab.basicsofjava;
+
 import java.util.Scanner;
 import java.util.Random;
 
 public class MenuDriven {
+	
+	public static Scanner sc = new Scanner(System.in);
+	public static int randNo = 0, option = 0, nooftry = 0;
+
 	public static void main(String args[]) {
 		Random rand = new Random();
-		int randNo = 0, option = 0, nooftry = 0;
 		randNo = rand.nextInt(10);
-		
 		display();
-		
 	}
-	public static void start(int randNo, int nooftry) {
-		
-		Scanner sc = new Scanner(System.in);
+
+	public static void display() {
+		final int Start = 1, Stop = 2, Quit = 3;
+		System.out.println("Menu:");
+		System.out.println("1.Start");
+		System.out.println("2.Stop");
+		System.out.println("3.Quit");
+		System.out.println("Enter any option from above");
+		option = sc.nextInt();
+		do {
+			switch (option) {
+			case Start:
+				start();
+				break;
+			case Stop:
+				stop();
+				break;
+			case Quit:
+				System.out.println("Quitting the program");
+				break;
+			default:
+				System.out.println("Plase enter valid option");
+				break;
+			}
+
+		} while (option != 3);
+	}
+
+	public static void start() {
 		System.out.println("enter any no between 0 to 10 ");
 		int Guess = sc.nextInt();
 		System.out.print(Guess);
@@ -27,8 +55,7 @@ public class MenuDriven {
 		}
 	}
 
-	public static void stop(int randNo,int nooftry) {
-		Scanner sc = new Scanner(System.in);
+	public static void stop() {
 		boolean win = false;
 		while (win == false) {
 			System.out.println("enter any no between 0 to 10 ");
@@ -37,7 +64,6 @@ public class MenuDriven {
 			nooftry++;
 			if (Guess == randNo) {
 				win = true;
-				// System.out.println("guess right in no of try:" + nooftry);
 			} else if (Guess < randNo) {
 				System.out.println("your guess is too low");
 			} else if (Guess > randNo) {
@@ -50,34 +76,5 @@ public class MenuDriven {
 		System.out.println("It took the no of try as :" + nooftry);
 
 	}
-	public static void display()
-	{
-		int randNo = 0, option = 0, nooftry = 0;
-		final int start = 1, stop = 2;
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Menu:");
-		System.out.println("1.Display");
-		System.out.println("2.Start");
-		System.out.println("3.stop");
-		System.out.println("Enter any option from above");
-		option = sc.nextInt();
-		do {
-			switch (option) {
-			case start:
-				start(randNo,nooftry);
-				break;
-			case stop:
-				stop(randNo,nooftry);
-				break;
-			default:
-				System.out.println("Plase enter valid option");
-				break;
-			}
-
-		} while (option != 3);
-	}
 
 }
-
-
-
