924f009cdde016a500c4f7d8d2399cb5683e5c19
sonamsingh15 <sonamsingh15>
2019-10-23 16:07:10 +0530

    java/

---
+++ java/welcome-java/src/com/techlab/basicsofjava/CodeMagnet3.java
@@ -0,0 +1,5 @@
+package com.techlab.basicsofjava;
+
+public class CodeMagnet3 {
+
+}
--- java/welcome-java/src/com/techlab/basicsofjava/CodeMagnets2.java
+++ java/welcome-java/src/com/techlab/basicsofjava/CodeMagnets2.java
@@ -1,30 +1,28 @@
 package com.techlab.basicsofjava;
 
 public class CodeMagnets2 {
-	public static void main(String [] args)
-	{
-		drumkit d=new drumkit();
-		d.snare=false;
+	public static void main(String[] args) {
+		drumkit d = new drumkit();
+		d.snare = false;
 		d.playSnare();
 		d.playTopHot();
-		if(d.snare==true)
-		{
+		if (d.snare == true) {
 			d.playSnare();
 		}
-		
+
 	}
-	
+
 }
-class drumkit
-{
-	boolean topat=true;
-	boolean snare=true;
-	public static void playTopHot()
-	{
+
+class drumkit {
+	boolean topat = true;
+	boolean snare = true;
+
+	public static void playTopHot() {
 		System.out.println("DING DING DE-DING");
 	}
-	public static void playSnare()
-	{
+
+	public static void playSnare() {
 		System.out.println("bang bang ba bang");
 	}
 
