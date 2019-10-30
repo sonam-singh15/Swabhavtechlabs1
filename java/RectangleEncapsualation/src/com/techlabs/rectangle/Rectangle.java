966d00a24048f07941f06d259d7857168809f4cc
sonam15 <sona12sing@gmail.com>
2019-10-30 17:07:09 +0530

    java/

--- java/RectangleEncapsualation/src/com/techlabs/rectangle/Rectangle.java
+++ java/RectangleEncapsualation/src/com/techlabs/rectangle/Rectangle.java
@@ -1,55 +1,50 @@
 package com.techlabs.rectangle;
 
 public class Rectangle {
-	
+
 	private int height;
 	private int width;
-	
-	public int getHeight()
-	{
-		
+
+	public int getHeight() {
+
 		return height;
 	}
-	public void setheight(int newheight)
-	{
-		/*if(newheight>100)
-		{
-	      newheight=100;
-	 System.out.println("you are exceeding the limit value of height will be reset to 100");
+
+	public void setheight(int newheight) {
+		
+		if (newheight < 0 ) {
+			newheight = 1;
+			System.out.println("you are using an negative number,it will be reset to 1");
 		}
-		height=newheight;*/
-		if(newheight<0) {
-			newheight=1;
-	   System.out.println("you are using an negative number,it will be reset to 1");
-		}
-		height=newheight;
+		height = newheight;
 	}
-	public int getwidth()
-	{
+
+	public int getwidth() {
 		return width;
 	}
-	public void setwidth(int newwidth)
-	{
-		/*if(newwidth>100)
-			newwidth=100;
-	   System.out.println("you are exceeding the limit value of width will be reset to 100");
-		width=newwidth;*/
-		if(newwidth<0) {
-			newwidth=1;
-		System.out.println("you are using an negative number,it will be reset to 1");
+
+	public void setwidth(int newwidth) {
+		
+		if (newwidth < 0) {
+			newwidth = 1;
+			System.out.println("you are using an negative number,it will be reset to 1");
 		}
-		width=newwidth;
+		width = newwidth;
 	}
-	
-	public int calculateArea()
-	{
+
+	public int calculateArea() {
 		int area;
 		// height=height>100?100:height;
 		// width=width>100?100:width;
-		 height=height<0?1:height;
-		 width=width<0?1:width;
-		area=height*width;
+		height = height < 0 ? 1 : height;
+		width = width < 0 ? 1 : width;
+		area = height * width;
 		return area;
 	}
 
 }
+/*
+ * if(newheight>100) { newheight=100; System.out.
+ * println("you are exceeding the limit value of height will be reset to 100");
+ * } height=newheight;
+ */
--- java/RectangleEncapsualation/src/com/techlabs/rectangletest/RectangleTest.java
+++ java/RectangleEncapsualation/src/com/techlabs/rectangletest/RectangleTest.java
@@ -3,25 +3,23 @@ package com.techlabs.rectangletest;
 import com.techlabs.rectangle.Rectangle;
 
 public class RectangleTest {
-	
-	public static void main(String [] args)
-	{
-		Rectangle small= new Rectangle();
+
+	public static void main(String[] args) {
+		Rectangle small = new Rectangle();
 		System.out.println("small rectangle details are");
 		small.setheight(-6);
-		small.setwidth(700);
-		System.out.println("height " +small.getHeight());
-		System.out.println("width " +small.getwidth());
-		System.out.println("area of small rectangle " +small.calculateArea());
-		Rectangle big=new Rectangle();
+		small.setwidth(100);
+		System.out.println("height " + small.getHeight());
+		System.out.println("width " + small.getwidth());
+		System.out.println("area of small rectangle " + small.calculateArea());
+		Rectangle big = new Rectangle();
 		System.out.println("big rectangle details are");
 		big.setheight(-20);
 		big.setwidth(100);
-		System.out.println("height " +big.getHeight());
-		System.out.println("width " +big.getwidth());
-		System.out.println("area of big rectangle" +big.calculateArea());
-				
-		
+		System.out.println("height " + big.getHeight());
+		System.out.println("width " + big.getwidth());
+		System.out.println("area of big rectangle" + big.calculateArea());
+
 	}
 
 }
