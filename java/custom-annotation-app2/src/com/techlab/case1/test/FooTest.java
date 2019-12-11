71d28642a165e74552f0c4350c9f44fce994e33b
sonam <sona12sing@gmail.com>
2019-12-11 09:08:45 +0530

    java/

--- java/custom-annotation-app2/src/com/techlab/case1/test/FooTest.java
+++ java/custom-annotation-app2/src/com/techlab/case1/test/FooTest.java
@@ -1,6 +1,7 @@
 package com.techlab.case1.test;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import com.techlab.case1.Foo;
@@ -8,17 +9,20 @@ import com.techlab.case1.RequireRefacatoring;
 
 public class FooTest {
 	public static int annocount = 0;
+	static int i = 0;
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+			throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		Foo obj = new Foo();
 		Class<? extends Foo> cls = obj.getClass();
 		Method[] methods = cls.getMethods();
+		for (Method m : methods) {
+			if (m.getDeclaredAnnotation(RequireRefacatoring.class) != null) {
 
-		for (Method method : methods) {
-			if (method.getAnnotations().equals("RequireRefacatoring")) {
-				annocount++;
+				i++;
 			}
-			System.out.println("methods" + method.getName() + " " + annocount);
+			System.out.println("Annoted Methods: ");
+			System.out.println(m.getName() + "count" + i);
 		}
 	}
 }
